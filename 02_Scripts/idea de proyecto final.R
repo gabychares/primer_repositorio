aad8d1c878b6be1758fe280c727628267e131d42
Mariana Gómez Becerra <mgomez110@alumnos.uaq.mx>
2025-11-10 10:21:28 -0600

    creación de archivo con base de datos

--- 02_Scripts/idea de proyecto final.R
+++ 02_Scripts/idea de proyecto final.R
@@ -2,7 +2,7 @@
 # Se tiene como idea que a partir de una función en la cual el usuario introduzca los datos de las variables 
 # que esta estudiando se determine el análisis estadístico que mejor se acople.
 
-maga <- function(){
+maga <- function() {
   readline (prompt = "¿Qué tipo de variable dependiente estás usando (continua/discreta)?") -> dependiente
   readline (prompt = "¿Qué tipo de variable independiente estás usando? (continua/discreta)") -> independiente
   readline (prompt = "¿Estás usando una covariable? (S/N)") -> covariable
@@ -19,3 +19,5 @@ maga <- function(){
 
 # Después se hace el análisis
   
+ChickWeight <- ChickWeight
+write.csv (ChickWeight, file = "01_Raw data/ChickWeight.csv")
